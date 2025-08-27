407b310bef3eaa7312831fca699ddcadf5c00f3c
unknown <alidargahi1368@gmail.com>
2025-08-27 13:03:28 +0330

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,47 +26,47 @@ export const metadata: Metadata = {
 const iranYekan = localFont({
   src: [
     {
-      path: '../../public/assets/fonts/iranyekan/woff2/IRANYekanXFaNum-Thin.woff2',
+      path: '../../public/assets/fonts/iranyekan/Woff2/IRANYekanXFaNum-Thin.woff2',
       weight: '100',
       style: 'normal',
     },
     {
-      path: '../../public/assets/fonts/iranyekan/woff2/IRANYekanXFaNum-UltraLight.woff2',
+      path: '../../public/assets/fonts/iranyekan/Woff2/IRANYekanXFaNum-UltraLight.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '../../public/assets/fonts/iranyekan/woff2/IRANYekanXFaNum-Light.woff2',
+      path: '../../public/assets/fonts/iranyekan/Woff2/IRANYekanXFaNum-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../../public/assets/fonts/iranyekan/woff2/IRANYekanXFaNum-Regular.woff2',
+      path: '../../public/assets/fonts/iranyekan/Woff2/IRANYekanXFaNum-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/assets/fonts/iranyekan/woff2/IRANYekanXFaNum-Medium.woff2',
+      path: '../../public/assets/fonts/iranyekan/Woff2/IRANYekanXFaNum-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../public/assets/fonts/iranyekan/woff2/IRANYekanXFaNum-DemiBold.woff2',
+      path: '../../public/assets/fonts/iranyekan/Woff2/IRANYekanXFaNum-DemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../../public/assets/fonts/iranyekan/woff2/IRANYekanXFaNum-Bold.woff2',
+      path: '../../public/assets/fonts/iranyekan/Woff2/IRANYekanXFaNum-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../../public/assets/fonts/iranyekan/woff2/IRANYekanXFaNum-ExtraBold.woff2',
+      path: '../../public/assets/fonts/iranyekan/Woff2/IRANYekanXFaNum-ExtraBold.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: '../../public/assets/fonts/iranyekan/woff2/IRANYekanXFaNum-Black.woff2',
+      path: '../../public/assets/fonts/iranyekan/Woff2/IRANYekanXFaNum-Black.woff2',
       weight: '900',
       style: 'normal',
     },
