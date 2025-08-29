c67a28850fb669ca5a46ee2bf562055916807ac3
unknown <alidargahi1368@gmail.com>
2025-08-29 21:34:50 +0330

    fix responsive bugs

--- src/features/landing/components/FourthSection/components/FourthSectionItem.tsx
+++ src/features/landing/components/FourthSection/components/FourthSectionItem.tsx
@@ -10,7 +10,7 @@ export default function Index({ title, id, imgUrl }: any) {
         <>
 
             {isMobile ? (
-                <>
+                <Link href={`/news/` + id}>
                     <section className='group flex flex-col items-center justify-center w-full h-[200px]  rounded-2xl bg-white hover:bg-[#7BB28033] group  transition-all duration-500 cursor-pointer'>
                         <section className='flex flex-row justify-between items-center p-1'>
                             <p className='font-bold text-xs block w-full'>{title}</p>
@@ -22,21 +22,21 @@ export default function Index({ title, id, imgUrl }: any) {
                             </Link>
                         </section>
                     </section>
-                </>
+                </Link>
             ) : (
-                <>
+                <Link href={`/news/` + id}>
                     <section className='group flex flex-col items-center justify-center p-2 w-[300px] h-[300px] xl:w-[365px] xl:h-[365px] rounded-2xl bg-white hover:bg-[#7BB28033] group  transition-all duration-500 cursor-pointer'>
                         <section className='flex flex-row justify-between items-center p-4'>
                             <p className='w-[85%] font-bold text-sm'>{title}</p>
-                            <Link href={`/news/${id}`} className='w-[38px] h-[38px] p-2 flex justify-center items-center rounded-md bg-[#35663A] text-white group-hover:bg-[#E9AA1E]  transition-all duration-500 cursor-pointer'>
+                            <div className='w-[38px] h-[38px] p-2 flex justify-center items-center rounded-md bg-[#35663A] text-white group-hover:bg-[#E9AA1E]  transition-all duration-500 cursor-pointer'>
                                 <ArrowRTLIcon className="w-[10px]" />
-                            </Link>
+                            </div>
                         </section>
                         <section className='relative w-[90%] mx-auto h-[240px] bg-cover' style={{ backgroundImage: imgUrl }}>
                             <Image src={imgUrl} alt={title} fill />
                         </section>
                     </section>
-                </>
+                </Link>
             )
             }
         </>
