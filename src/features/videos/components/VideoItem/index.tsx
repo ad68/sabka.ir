621935cfc37db1474eec07992d1006e0123aa1b7
unknown <alidargahi1368@gmail.com>
2025-09-03 10:02:22 +0330

    improve video box in video gallery

--- src/features/videos/components/VideoItem/index.tsx
+++ src/features/videos/components/VideoItem/index.tsx
@@ -1,15 +1,20 @@
 import { PlayCircleIcon } from "lucide-react";
 import Image from "next/image";
-export default function Index({ playVideo, thumbSrc, videoSrc, date }: any) {
-    return <div onClick={() => playVideo(videoSrc)} className="w-full relative  cursor-pointer group transition-all duration-300 ease-in-out min-h-2">
+export default function Index({ playVideo, thumbSrc, videoSrc, date, title }: any) {
+    return <div onClick={() => playVideo(videoSrc)} className="w-full relative border-t-[2px] border-primary p-3 rounded-lg py-6 bg-white shadow-sm hover:shadow-xl cursor-pointer group transition-all duration-300 ease-in-out min-h-2">
         <Image width={400} height={400} className="w-full rounded-[10px]" src={thumbSrc} alt="" />
-        <div className="absolute top-0 w-[200ppx] bg-[#00000085] z-50 text-white rounded-br-2xl left-0 px-4 py-1 text-[12px]">{date}</div>
         <div className="w-full mt-[60px] flex justify-center items-center top-0 absolute">
             <PlayCircleIcon className="w-[100px] text-secondary group-hover:text-black transition-all duration-300 h-[100px]" />
         </div>
         <div>
-            <div className="bg-primary text-white rounded-xl mt-1  px-3 py-2 text-sm">پوستر موشن چهل و یکمین سالگرد تأسیس صندوق بیمه کشاورزی</div>
-
+            <div className="text-primary gap-2 rounded-xl flex items-center mt-1 px-3 py-2  text-sm font-bold">
+                <PlayCircleIcon className="w-[30px] text-secondary group-hover:text-black transition-all duration-300" />
+                <p className="leading-6">
+                    {title}
+                </p>
+            </div>
         </div>
+
+        <div className="text-xs flex justify-end px-3 mt-2 font-bold">{date}</div>
     </div>
 }
--- src/features/videos/components/Videos.tsx
+++ src/features/videos/components/Videos.tsx
@@ -15,12 +15,12 @@ export default function Index() {
         <section className="max-w-6xl m-auto p-2 mb-[40px]">
             <h1 className="font-bold text-center mb-5 text-xl text-primary mt-[40px]">گالری فیلم</h1>
             <section className="grid grid-cols-1 gap-3 xl:grid-cols-3">
+                <VideoItem date="1404/06/11 | 20:20" playVideo={playVideo} videoSrc={"/videos/film1.mp4"} thumbSrc="/videos/thumb1.png" title="پوستر موشن چهل و یکمین سالگرد تأسیس صندوق بیمه کشاورزی" />
+                {/*  <VideoItem date="1404/06/11 | 20:20" playVideo={playVideo} videoSrc={"/videos/film1.mp4"} thumbSrc="/videos/thumb1.png" /> */}
+                {/*      <VideoItem date="1404/06/11 | 20:20" playVideo={playVideo} videoSrc={"/videos/film1.mp4"} thumbSrc="/videos/thumb1.png" />
                 <VideoItem date="1404/06/11 | 20:20" playVideo={playVideo} videoSrc={"/videos/film1.mp4"} thumbSrc="/videos/thumb1.png" />
                 <VideoItem date="1404/06/11 | 20:20" playVideo={playVideo} videoSrc={"/videos/film1.mp4"} thumbSrc="/videos/thumb1.png" />
-                <VideoItem date="1404/06/11 | 20:20" playVideo={playVideo} videoSrc={"/videos/film1.mp4"} thumbSrc="/videos/thumb1.png" />
-                <VideoItem date="1404/06/11 | 20:20" playVideo={playVideo} videoSrc={"/videos/film1.mp4"} thumbSrc="/videos/thumb1.png" />
-                <VideoItem date="1404/06/11 | 20:20" playVideo={playVideo} videoSrc={"/videos/film1.mp4"} thumbSrc="/videos/thumb1.png" />
-                <VideoItem date="1404/06/11 | 20:20" playVideo={playVideo} videoSrc={"/videos/film1.mp4"} thumbSrc="/videos/thumb1.png" />
+                <VideoItem date="1404/06/11 | 20:20" playVideo={playVideo} videoSrc={"/videos/film1.mp4"} thumbSrc="/videos/thumb1.png" /> */}
             </section>
         </section>
         <CustomModal width={1000} open={openPlayer} onClose={() => setOpenPlayer(false)}>
