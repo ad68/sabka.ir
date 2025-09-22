9dbfaab60831c90af4ff73f6391611d9e3f15c1e
unknown <alidargahi1368@gmail.com>
2025-09-22 23:52:44 +0330

    .

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next'
-import Login from '@/features/Login/components/Login'
+import Login from '@/features/MyLogin/components/Login'
 const Index: NextPage = () => {
     return <Login />
 }
